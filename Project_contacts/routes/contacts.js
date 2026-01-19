e4b090c7ea28ca8aa5d3981283c5dd2e2eb3fe14
jordansanchez23 <joivisvis@hotmail.com>
2026-01-18 19:04:42 -0800

    Create, Update & Delete routes've been made

--- Project_contacts/routes/contacts.js
+++ Project_contacts/routes/contacts.js
@@ -7,4 +7,10 @@ router.get('/', contactsController.getAll);
 
 router.get('/:id', contactsController.getSingle);
 
+router.post('/', contactsController.createUser);
+
+router.put('/:id', contactsController.updateUser);
+
+route.delete('/:id', contactsController.deleteUser);
+
 module.exports = router;
