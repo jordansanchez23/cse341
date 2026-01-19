6304f14e3d129782e6842d4a19362867af40372f
jordansanchez23 <joivisvis@hotmail.com>
2026-01-18 22:38:46 -0800

    swagger added

--- Project_contacts/server.js
+++ Project_contacts/server.js
@@ -16,6 +16,16 @@ port = process.env.PORT || 3000;
  * Middleware
  *************************/
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-width, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
+
 app.use('/', require('./routes'));
 
 
