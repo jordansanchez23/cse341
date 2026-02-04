099d2bed5798215a988c0fb238de09cdb30491b0
jordansanchez23 <joivisvis@hotmail.com>
2026-02-03 20:26:41 -0800

    Packages, Middleware and passport added

--- Project_LDC/server.js
+++ Project_LDC/server.js
@@ -4,6 +4,12 @@
 const express = require("express");
 const mongoDb = require('./data/database');
 const bodyParser = require('body-parser');
+const passport = require("passport");
+const session = require("express-session");
+const GitHubStrategy = require("passport-github2").Strategy;
+const cors = require("cors");
+
+
 const app = express()
 
 
@@ -15,18 +21,43 @@ port = process.env.PORT = 5500;
 /* ***********************
  * Middleware
  *************************/
-app.use(bodyParser.json());
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-width, Content-Type, Accept, Z-Key'
-    );
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    next();
-});
+app
+    .use(bodyParser.json())
+    .use(session({
+        secret: "secret",
+        resave: false,
+        saveUninitialized: true,
+    }))
+    //This is the basic express session ({..}) initialization.
+    .use(passport.initialize())
+    //init passport on every route call.
+    .use(passport.session())
+    //allow passport to use "express-session".
+    .use((req, res, next) => {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader(
+            "Access-Control-Allow-Headers",
+            "Origin, X-Requested-With, Content-Type, Accept, Z-Key, Authorization"
+        );
+        res.setHeader(
+            "Access-Control-Allow-Methods",
+            "POST, GET, PUT, PATCH, OPTIONS, DELETE"
+        );
+        next();
+    })
+    .use(cors({ methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH' ]}))
+    .use(cors({ origin: '*'}))
+    .use("/", require("./routes/index.js"));
 
-app.use('/', require('./routes'));
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.CALLBACK_URL
+},
+function(accessToken, refreshToken, profile, done) {
+    return done(null, profile);
+}
+));
 
 
 /* ***********************
